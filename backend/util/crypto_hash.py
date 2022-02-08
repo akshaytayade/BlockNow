e016a734a75f37716fbfff990f811436fad18e8c
akshaytayade <tayade_akshay@outlook.com>
2022-02-08 09:09:26 +0530

    added_wallet_transactions

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -2,22 +2,17 @@ import hashlib
 import json
 
 def crypto_hash(*args):
-
-    stringified_args=sorted(map(lambda data: json.dumps(data),args))
-
-    print(f'stringified_args: {args}')
-
-    joined_data=''.join(stringified_args)
-      
-    print(f'joined_data:{joined_data}')
+    """
+    Return a sha-256 hash of the given arguments.
+    """
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = ''.join(stringified_args)
 
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash('one',2,[3]):{crypto_hash('one',2,[3])}")
-    print(f"crypto_hash(1,'two',[3]):{crypto_hash(1,'two',[3])}")
-    print(f"crypto_hash(1,2,[3]):{crypto_hash(1,2,[3])}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
+    print(f"crypto_hash(2, 'one', [3]): {crypto_hash(2, 'one', [3])}")
 
-
-if __name__== "__main__":
+if __name__ == '__main__':
     main()
